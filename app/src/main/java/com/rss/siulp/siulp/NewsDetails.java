79123d2ebcdd56c4d0148107dec12cbe79f32759
mtamagno <thegreatherochrono@hotmail.it>
2016-12-11 21:27:29 +0100

    Javascript enabled

--- app/src/main/java/com/rss/siulp/siulp/NewsDetails.java
+++ app/src/main/java/com/rss/siulp/siulp/NewsDetails.java
@@ -1,8 +1,12 @@
 package com.rss.siulp.siulp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class NewsDetails extends AppCompatActivity {
 WebView webView;
@@ -10,8 +14,29 @@ WebView webView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
-        webView = (WebView) findViewById(R.id.webView);
         Bundle bundle = getIntent().getExtras();
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings =  webView.getSettings();
+        webView.getSettings().setJavaScriptEnabled (true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webView.getSettings().setSupportMultipleWindows (false);
+        webView.getSettings().setSupportZoom (false);
+        webView.setVerticalScrollBarEnabled (false);
+        webView.setHorizontalScrollBarEnabled (false);
         webView.loadUrl(bundle.getString("Link"));
+
+        webView.setWebViewClient(new WebViewClient() {
+            @ Override public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("vnd.youtube")){
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/rss/siulp/siulp/RSSActivity.java
+++ app/src/main/java/com/rss/siulp/siulp/RSSActivity.java
@@ -1,6 +1,10 @@
 package com.rss.siulp.siulp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +15,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class RSSActivity extends AppCompatActivity {
+    boolean ancona,italia;
     RecyclerView recyclerView;
+    ReadRSS readRSS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadPreferences();
         setContentView(R.layout.activity_rss);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -23,13 +30,24 @@ public class RSSActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.tamagno.it/"));
+                startActivity(browserIntent);
             }
         });
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        ReadRSS readRSS = new ReadRSS(this,recyclerView);
+        readRSS = new ReadRSS(this,recyclerView,ancona,italia);
         readRSS.execute();
+    }
+
+    @Override
+    public void onRestart()
+    {  // After a pause OR at startup
+        super.onRestart();
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+       // readRSS.cancel(true);
+       // readRSS.execute();
     }
 
     @Override
@@ -48,9 +66,21 @@ public class RSSActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AppPreferences.class);
+            startActivity(intent);
             return true;
         }
-
+        if(id == R.id.action_info){
+            Intent intent = new Intent(this, AboutListActivity.class);
+            startActivity(intent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadPreferences(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        ancona = sharedPreferences.getBoolean("ancona",true);
+        italia = sharedPreferences.getBoolean("italia",true);
     }
 }
